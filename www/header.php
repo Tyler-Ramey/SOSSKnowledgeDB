0ca4313ad73f82470161e47b9e8bf6f2581fd86c
Tyler Ramey <tyramey95@gmail.com>
2023-08-05 21:24:11 -0400

    Updated file paths and added logo

--- www/header.php
+++ www/header.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Knowledge Base</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="..\config\styles.css">
 </head>
 <body>
     <header>
         <div class="container">
-            <a href="#" class="logo">Your Logo</a>
+            <a href="#" class="logo">
+                <img src="logo.png" alt="SOSS Logo">
+            </a>
             <nav>
                 <input type="text" id="search-bar" placeholder="Search...">
                 <a href="#" class="home-btn">Home</a>
