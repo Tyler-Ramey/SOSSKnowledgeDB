ad45a1592e915c16cb9630f718fc9b5d261f5a47
Tyler Ramey <tyramey95@gmail.com>
2023-08-06 14:27:40 -0400

    Removed lines of code that were not needed in header and footer

--- www/footer.php
+++ www/footer.php
@@ -1,12 +1,5 @@
-        </div>
-    </main>
-
     <footer>
         <div class="container">
             Designed by T. Ramey for INFR 495
         </div>
     </footer>
-
-    <script src="script.js"></script>
-</body>
-</html>
--- www/header.php
+++ www/header.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Knowledge Base</title>
-    <link rel="stylesheet" href="..\config\styles.css">
-</head>
-<body>
     <header>
         <div class="container">
             <a href="#" class="logo">
@@ -14,10 +5,7 @@
             </a>
             <nav>
                 <input type="text" id="search-bar" placeholder="Search...">
-                <a href="#" class="home-btn">Home</a>
+                <a href="index.php" class="home-btn">Home</a>
             </nav>
         </div>
     </header>
-
-    <main>
-        <div class="container">
