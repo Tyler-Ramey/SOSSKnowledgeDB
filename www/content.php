6d5c228c5b0b96b531ca52a1f28710c1b4c0d73e
Tyler Ramey <tyramey95@gmail.com>
2023-08-06 14:13:23 -0400

    First working commit of content file

---
+++ www/content.php
@@ -0,0 +1,34 @@
+<?php
+// Include the database connection file
+require_once 'db_connect.php';
+
+// Check if the ID is passed from the link
+if (isset($_GET['id'])) {
+    // Sanitize the ID to prevent SQL injection
+    $id = intval($_GET['id']);
+
+    try {
+        // Query the database to retrieve the content based on the ID
+        $sql = "SELECT * FROM knowledge_base WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        // Check if the entry with the given ID exists
+        if ($stmt->rowCount() > 0) {
+            // Display the content
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            echo "<div class='entry'>";
+            echo "<h2>" . $row['title'] . "</h2>";
+            echo $row['content'];
+            echo "</div>";
+        } else {
+            echo "Entry not found.";
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
