547615611d686cba9b6e5d2c548a820056aea520
Tyler Ramey <tyramey95@gmail.com>
2023-08-06 14:14:04 -0400

    removed unused variables

--- www/db_connect.php
+++ www/db_connect.php
@@ -1,11 +1,7 @@
 <?php
+// Database connection
 try {
-    $servername = "localhost";
-    $username = "tramey";
-    $password = "TMRtmr2021!";
-    $dbname = "soss_knowledge_base";
-
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $pdo = new PDO('mysql:host=localhost;dbname=soss_knowledge_base', 'tramey', 'TMRtmr2021!');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET NAMES "utf8"');
 } catch (PDOException $e) {
