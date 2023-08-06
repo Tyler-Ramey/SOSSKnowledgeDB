4f15a0ad6c800620d7c3bcfc3451387cff783669
Tyler Ramey <tyramey95@gmail.com>
2023-08-05 21:24:31 -0400

    First commit of JS to run tabls

---
+++ www/tabs.js
@@ -0,0 +1,17 @@
+// JavaScript for tab functionality
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
+
+// Open the first tab by default
+document.getElementsByClassName("tablinks")[0].click();
