811e419f0d5ceaa379810f6b59178528194614a5
Tyler Ramey <tyramey95@gmail.com>
2023-08-07 22:38:18 -0400

    Updated header to working header

--- www/header.php
+++ www/header.php
@@ -1,10 +1,13 @@
-    <header>
-        <div class="container">
+<header>
+        <div class="header_container">
             <a href="#" class="logo">
                 <img src="logo.png" alt="SOSS Logo">
             </a>
             <nav>
-                <input type="text" id="search-bar" placeholder="Search...">
+                <form action="search.php" method="get">
+                    <input type="text" id="search-bar" name="q" placeholder="Search...">
+                    <button type="submit">Search</button>
+                </form>
                 <a href="index.php" class="home-btn">Home</a>
             </nav>
         </div>
