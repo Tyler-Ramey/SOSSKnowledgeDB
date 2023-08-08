fea8c9d5301ff49ee5fff995c4164f3916e42e5f
Tyler Ramey <tyramey95@gmail.com>
2023-08-07 22:45:45 -0400

    Added an include statement for the header

--- www/index.php
+++ www/index.php
@@ -23,20 +23,7 @@ function getTabContent($pdo, $tabName) {
 ?>
 
 <!-- Header -->
-<header>
-        <div class="header_container">
-            <a href="#" class="logo">
-                <img src="logo.png" alt="SOSS Logo">
-            </a>
-            <nav>
-                <form action="search.php" method="get">
-                    <input type="text" id="search-bar" name="q" placeholder="Search...">
-                    <button type="submit">Search</button>
-                </form>
-                <a href="index.php" class="home-btn">Home</a>
-            </nav>
-        </div>
-    </header>
+<?php include 'header.php'; ?>
 
 <!-- Main content -->
 <main>
